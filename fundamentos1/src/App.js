b9a56f131b0abb676bfedaf11dba5b596233f321
ashhsay <ashley.alves@etec.sp.gov.br>
2021-05-11 01:00:19 +0000

    aula 1 fundamentos do react

---
+++ fundamentos1/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import olamundo from './components/basico/olamundo'
+class App extends Component {
+  render () {
+    return(
+      <div>
+        <h1>Aula 01-Fundamentos</h1>
+
+        <olamundo />
+
+      </div>
+
+    )
+  }
+
+}
+
+export default App;
---
+++ fundamentos1/src/components/basico/olamundo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default () => {
+    
+    
+    const informacao = 'Primeiro componente JSX'
+
+    return (
+        <>
+
+        <h2>Componente olamundo</h2>
+        <p> { informacao } </p>
+        </>
+    )
+
+}
