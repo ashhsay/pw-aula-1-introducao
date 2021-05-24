3b21839bea82d7c5e80be76d114003f23a0f063b
ashhsay <ashley.alves@etec.sp.gov.br>
2021-05-24 22:39:44 +0000

    aula 2 2° comimit
    Signed-off-by: ashhsay <ashley.alves@etec.sp.gov.br>

--- fundamentos1/src/App.js
+++ fundamentos1/src/App.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react'
 
-import olamundo from './components/basico/olamundo'
-import Propriedade from '/workspace/pw-aula-1-introducao/fundamentos1./src/app.js/components/basico/Propriedade'
+import Card from '/components/layout/Card'
+import olamundo from './components/basico/olamundo';
+import Propriedade from './components/basico/Propriedades';
 
 class App extends Component {
   render () {
     return(
       <div>
         <h1> Propriedade </h1>
-        <h1>Aula 01-Fundamentos</h1>
-        <h2>Olá mundo</h2>
         <Propriedade nome="homer"  sobrenome="Jay Simpson" idade={38}/>
-        <olamundo />
 
+        <h1>Aula 01-Fundamentos</h1>
+        <h2> Componente olamundo </h2>
+        <h4>primeiro componente JSX</h4>
+        <olamundo />
+        
       </div>
 
     )
